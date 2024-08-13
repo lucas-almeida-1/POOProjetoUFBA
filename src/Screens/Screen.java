f023ba551d01feda978af724b9d3be089643e149
thalesleo <thalesleo@gmail.com>
2024-08-13 00:15:42 -0300

    att

---
+++ src/Screens/Screen.java
@@ -0,0 +1,12 @@
+package Screens;
+
+import javax.swing.*;
+
+public class Screen {
+    public Screen(){
+        JFrame jFrame = new JFrame();
+        jFrame.setVisible(true);
+        jFrame.setSize(800, 500);
+        jFrame.setTitle("Steam Verde");
+    }
+}
--- src/Start/Start.java
+++ src/Start/Start.java
@@ -4,13 +4,13 @@ import Entidade.Perfil;
 import Entidade.Usuario;
 import Entidade.Conquistas;
 import Entidade.Item;
-
+import Screens.Screen;
 public class Start {
     public static void main(String[] args) {
         Perfil perfil = new Perfil("user", "senha", "email", "apelido", new Usuario[0], new Conquistas[0], new Item[0]);
         Usuario usuario = new Usuario("user", "senha", "email");
         System.out.println("Perfil: " + perfil.getUsername() + " " + perfil.getSenha() + " " + perfil.getEmail() + " " + perfil.getApelido());
         System.out.println("Usuario: " + usuario.getUsername() + " " + usuario.getSenha() + " " + usuario.getEmail());
-
+        new Screen();
     }
 }
